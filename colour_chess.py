6882593eecf50512c5b39a3931e915bb824c3a7d
Toby Daines <dainest12@gmail.com>
2024-06-13 14:22:14 +0100

    Added valid rook moves

--- colour_chess.py
+++ colour_chess.py
@@ -134,9 +134,9 @@ def check_options(pieces, locations, turn):
         piece = pieces[i]
         if piece == 'pawn':
             moves_list = check_pawn(location, turn)
-        """elif piece == 'rook':
+        elif piece == 'rook':
             moves_list = check_rook(location, turn)
-        elif piece == 'knight':
+        """elif piece == 'knight':
             moves_list = check_knight(location, turn)
         elif piece == 'bishop':
             moves_list = check_bishop(location, turn)
@@ -146,6 +146,43 @@ def check_options(pieces, locations, turn):
             moves_list = check_king(location, turn)"""
         all_moves_list.append(moves_list)
     return all_moves_list
+
+
+# Check valid rook moves
+def check_rook(position, color):
+    moves_list = []
+    if color == 'white':
+        enemies_list = black_locations
+        friends_list = white_locations
+    else:
+        friends_list = black_locations
+        enemies_list = white_locations
+    for i in range(4): # Loop for checking down, up, left, right
+        path = True
+        chain = 1
+        if i == 0:
+            x = 0
+            y = 1
+        elif i == 1:
+            x = 0
+            y = -1
+        elif i == 2:
+            x = 1
+            y = 0
+        else:
+            x = -1
+            y = 0
+        while path:
+            if (position[0] + (chain * x), position[1] + (chain * y)) not in friends_list and \
+                    0 <= position[0] + (chain * x) <= 7 and 0 <= position[1] + (chain * y) <= 7:
+                moves_list.append((position[0] + (chain * x), position[1] + (chain * y)))
+                if (position[0] + (chain * x), position[1] + (chain * y)) in enemies_list:
+                    path = False
+                chain += 1
+            else:
+                path = False
+
+    return moves_list
 
 # Check valid pawn moves
 def check_pawn(position, color):
@@ -167,7 +204,7 @@ def check_pawn(position, color):
             moves_list.append((position[0], position[1] - 1))
         if (position[0], position[1] - 2) not in white_locations and \
                 (position[0], position[1] - 2) not in black_locations and position[1] == 6:
-            moves_list.append((position[0], position[1] + 2))
+            moves_list.append((position[0], position[1] - 2))
         if (position[0] + 1, position[1] - 1) in white_locations:
             moves_list.append((position[0] + 1, position[1] - 1))
         if (position[0] - 1, position[1] - 1) in white_locations:
