f9edef77acb364311f7132d940820635f782c0ad
Toby Daines <dainest12@gmail.com>
2024-07-17 17:04:28 +0100

    En passant valid move

--- colour_chess.py
+++ colour_chess.py
@@ -189,6 +189,11 @@ def check_pawn(position, color):
             moves_list.append((position[0] + 1, position[1] + 1))
         if (position[0] - 1, position[1] + 1) in black_locations:
             moves_list.append((position[0] - 1, position[1] + 1))
+        # Add en passant move checker
+        if (position[0] + 1, position[1] + 1) == black_ep:
+            moves_list.append((position[0] + 1, position[1] + 1))
+        if (position[0] - 1, position[1] + 1) == black_ep:
+            moves_list.append((position[0] - 1, position[1] + 1))
     else:
         if (position[0], position[1] - 1) not in white_locations and \
                 (position[0], position[1] - 1) not in black_locations and position[1] > 0:
@@ -199,6 +204,11 @@ def check_pawn(position, color):
         if (position[0] + 1, position[1] - 1) in white_locations:
             moves_list.append((position[0] + 1, position[1] - 1))
         if (position[0] - 1, position[1] - 1) in white_locations:
+            moves_list.append((position[0] - 1, position[1] - 1))
+        # Add en passant move checker
+        if (position[0] + 1, position[1] - 1) == white_ep:
+            moves_list.append((position[0] + 1, position[1] - 1))
+        if (position[0] - 1, position[1] - 1) == white_ep:
             moves_list.append((position[0] - 1, position[1] - 1))
     return moves_list
 
